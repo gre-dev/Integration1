7638dbe8905f448811c69cfd6136f3ee50362775
Usama Makhzoum <osmakh1@gmail.com>
2022-10-31 16:28:44 +0300

    add column time to accounts table (references #7)

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 31, 2022 at 03:51 PM
+-- Generation Time: Oct 31, 2022 at 04:28 PM
 -- Server version: 8.0.23-3+b1
 -- PHP Version: 8.1.3
 
@@ -32,7 +32,8 @@ CREATE TABLE `accounts` (
   `username` text NOT NULL,
   `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `password` text NOT NULL,
-  `reset_pass_token` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci
+  `reset_pass_token` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `time` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -51,6 +52,13 @@ CREATE TABLE `api_keys` (
   `date` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `api_keys`
+--
+
+INSERT INTO `api_keys` (`id`, `account_id`, `plan_id`, `api_key`, `requests`, `referrer`, `date`) VALUES
+(5, 12, 1, '635fcc8142b5f', 0, NULL, 1667222657);
+
 -- --------------------------------------------------------
 
 --
@@ -63,6 +71,13 @@ CREATE TABLE `api_keys_plans` (
   `max_requests` int NOT NULL,
   `period` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `api_keys_plans`
+--
+
+INSERT INTO `api_keys_plans` (`id`, `name`, `max_requests`, `period`) VALUES
+(1, 'l', 2, 2);
 
 --
 -- Indexes for dumped tables
@@ -95,19 +110,19 @@ ALTER TABLE `api_keys_plans`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `api_keys`
 --
 ALTER TABLE `api_keys`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `api_keys_plans`
 --
 ALTER TABLE `api_keys_plans`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
