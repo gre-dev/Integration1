8c79c755bc634cb52ee8d1ce6d828ef5cb83e3a0
Usama Makhzoum <osmakh1@gmail.com>
2022-11-01 20:43:53 +0300

    merge filter and revert input functions (reference #3)

--- Traits/InputHandleTrait.php
+++ Traits/InputHandleTrait.php
@@ -2,20 +2,16 @@
 
 trait InputHandleTrait {
     
-    private function filter_html_input($input) {
-        return htmlspecialchars($input);
+    private function filter_input($input) {
+        $filtered = htmlspecialchars($input);
+        $filtered = addslashes($filtered);
+        return $filtered;
     }
 
-    private function revert_html_filter($filtered) {
-        return htmlspecialchars_decode($filtered);
-    }
-
-    private function filter_sql_input($input) {
-        return addslashes($input);
-    }
-
-    private function revert_sql_input($filtered) {
-        return stripslashes($filtered);
+    private function revert_filtered($filtered) {
+        $input = stripslashes($filtered);
+        $input = htmlspecialchars_decode($input);
+        return $input;
     }
 }
 
